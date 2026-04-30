393ddb0bfd00a6e33f848425d8984b05b45a386e
TANISH816 <tanishmakwana1234@gmail.com>
2026-04-30 23:27:13 +0530

    Face detection done. api route ./api/faces/detect

--- LensLogic/src/main/java/com/shadow/LensLogic/LensLogicApplication.java
+++ LensLogic/src/main/java/com/shadow/LensLogic/LensLogicApplication.java
@@ -1,13 +1,23 @@
 package com.shadow.LensLogic;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.ai.vectorstore.pgvector.autoconfigure.PgVectorStoreAutoConfiguration.class
+})
 public class LensLogicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LensLogicApplication.class, args);
 	}
+
+    @PostConstruct
+    public void init() {
+        // This loads the shared OpenCV library into your JVM
+        nu.pattern.OpenCV.loadShared(); //[cite: 52]
+    }
 
 }
